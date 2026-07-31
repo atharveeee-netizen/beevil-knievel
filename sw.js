a3668205c5d6f126bfa794eb3d67abed8ba63f2b
Atharve Dahima <atharveeee@gmail.com>
2026-07-31 14:07:29 +0530

    Transform Beevil Knievel into a full commercial AgriTech startup product ecosystem (Store, PWA, Hardware Specs, Docs, Privacy)

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'beevil-knievel-v2.4';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './store.html',
+  './download.html',
+  './app.html',
+  './docs.html',
+  './privacy.html',
+  './manifest.json',
+  './assets/beevil_product_hero.jpg',
+  './assets/beevil_hardware_render.jpg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        if (networkResponse && networkResponse.status === 200 && networkResponse.type === 'basic') {
+          const responseToCache = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+        }
+        return networkResponse;
+      }).catch(() => {
+        return caches.match('./app.html');
+      });
+    })
+  );
+});
